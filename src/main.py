59815e4dc1c9043a1d0ad9ba530bdbd55f36c1e1
Jaivin Raj <jaivinraj@Jaivins-MBP.lan>
2022-04-14 23:45:22 +0100

    Fix dodgy formatting

--- src/main.py
+++ src/main.py
@@ -80,54 +80,55 @@ def get_new_path(fpath,processor,foldername = "soulseek",ext=None,fields = ["art
 
 
 def import_files(dir):
-        # get files
-        print("Getting files...")
-        fpaths = {ext:[] for ext in EXTS}
-        # get filepaths
-        for root, dirs, files in os.walk(dir):
-            for filename in files:
-                ext = None
-                for ext in EXTS:
-                    if filename.endswith(ext):
-                        fpaths[ext].append(os.path.join(root,filename))
-                        break
-        print("Found files with the following extensions:\n","\n".join([ext + ":" + str(len(fpaths[ext])) + '\n' for ext in fpaths]))
+    
+    # get files
+    print("Getting files...")
+    fpaths = {ext:[] for ext in EXTS}
+    # get filepaths
+    for root, dirs, files in os.walk(dir):
+        for filename in files:
+            ext = None
+            for ext in EXTS:
+                if filename.endswith(ext):
+                    fpaths[ext].append(os.path.join(root,filename))
+                    break
+    print("Found files with the following extensions:\n","\n".join([ext + ":" + str(len(fpaths[ext])) + '\n' for ext in fpaths]))
 
 
-        # print("found flac files:",",".join(fpaths[".flac"]]))
-                        
-        print("Creating mappings to new destinations...")
-        fpath_mappings = {}
-        for ext in fpaths:
-            for fpath in tqdm(fpaths[ext]):
-                fpath_mappings[fpath] = get_new_path(fpath,EXTS[ext],foldername=dir)
-        print("Moving files over...")
-        counter = 0
-        for fpath in tqdm(fpath_mappings):
-            dest = fpath_mappings[fpath]
-            if dest is not None:
-                # check haven't already imported
-                if not os.path.exists(dest):
-                    # removed as aiff not importing right now
-                    if fpath.endswith(".flac"):
-                        sound = AudioSegment.from_file(fpath)
-                        fileroot = os.path.splitext(fpath)[0]
-                        fpath_new = fileroot + ".aiff"
-                        sound.export(fpath_new, format="aiff")
-                        os.remove(fpath) # delete old file
-                        fpath = fpath_new # update to new path
-                        dest = os.path.splitext(dest)[0] + ".aiff" # update destination
-                    folder,filename = os.path.split(dest)
-                    if not os.path.exists(folder):
-                        os.makedirs(folder)
-                    move(fpath,dest)
-                    counter +=1
-                else:
-                    print(f"Not importing {dest} as it already exists")
-                    # remove
-                    os.remove(fpath)
-        
-        print("File import complete.",f"Imported {counter} out of {len(fpath_mappings)} files")
+    # print("found flac files:",",".join(fpaths[".flac"]]))
+                    
+    print("Creating mappings to new destinations...")
+    fpath_mappings = {}
+    for ext in fpaths:
+        for fpath in tqdm(fpaths[ext]):
+            fpath_mappings[fpath] = get_new_path(fpath,EXTS[ext],foldername=dir)
+    print("Moving files over...")
+    counter = 0
+    for fpath in tqdm(fpath_mappings):
+        dest = fpath_mappings[fpath]
+        if dest is not None:
+            # check haven't already imported
+            if not os.path.exists(dest):
+                # removed as aiff not importing right now
+                if fpath.endswith(".flac"):
+                    sound = AudioSegment.from_file(fpath)
+                    fileroot = os.path.splitext(fpath)[0]
+                    fpath_new = fileroot + ".aiff"
+                    sound.export(fpath_new, format="aiff")
+                    os.remove(fpath) # delete old file
+                    fpath = fpath_new # update to new path
+                    dest = os.path.splitext(dest)[0] + ".aiff" # update destination
+                folder,filename = os.path.split(dest)
+                if not os.path.exists(folder):
+                    os.makedirs(folder)
+                move(fpath,dest)
+                counter +=1
+            else:
+                print(f"Not importing {dest} as it already exists")
+                # remove
+                os.remove(fpath)
+    
+    print("File import complete.",f"Imported {counter} out of {len(fpath_mappings)} files")
 
 def cleanup_empty_folders(path):
     print("cleaning")
